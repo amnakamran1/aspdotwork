d944c59f45b33c80bbff53aac074eaf08b7de7d0
amnakamran1 <Amnakamran2024@gmail.com>
2024-12-13 22:55:41 -0800

    test

---
+++ wwwwgg/wwwwgg/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace wwwwgg.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
