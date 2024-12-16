cb8b761f2e1d1750543fb12c9cf6168d068c077d
amnakamran1 <Amnakamran2024@gmail.com>
2024-12-15 21:57:12 -0800

    viewbag & viewdata

---
+++ viewbag-viewdata/viewbag-viewdata/Controllers/viewController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using viewbag_viewdata.Models;
+
+namespace viewbag_viewdata.Controllers
+{
+    public class ViewController : Controller
+    {
+        public IActionResult view()
+        {
+            ViewBag.message = "Data come from view bag";
+            ViewData["messages2"] = "Data come from view data";
+            ViewBag.CurrentDate = DateTime.Now.ToString();
+            ViewData["CurrentDate"] = DateTime.Now.ToLongDateString();
+
+            string[] games = { "Cricket", "Football", "Hockey", "Baskitball" };
+            ViewBag.games1 = games;
+            ViewData["games2"] = games;
+
+            Student Amna = new Student();
+            Amna.StdId = 1;
+            Amna.StdName = "Amna kamran";
+            Amna.StdAddress = "Sector 5d";
+            ViewBag.Amnak = Amna;
+            ViewData["AmnaKamran"]=Amna;
+
+
+            return View();
+        }
+    }
+}
---
+++ viewbag-viewdata/viewbag-viewdata/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace viewbag_viewdata.Models
+{
+    public class Student
+    {
+        public int StdId { get; set; }
+        public string StdName { get; set; }
+        public string StdAddress { get; set; }
+
+    }
+}
